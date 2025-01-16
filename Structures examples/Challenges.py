a0b844009e6a295a52babaf4db21856e9228fe6e
JorgeBear <jorge.villalon.glez@gmail.com>
2025-01-16 19:38:41 +0100

    More

--- Structures examples/Challenges.py
+++ Structures examples/Challenges.py
@@ -20,6 +20,73 @@ resultado = is_sorted([4, 5, 6, 7, 8])
 print(resultado)
 
 
+#2 Convert binary number to  decimal
+#Write a function that converts a binary number to its decimal equivalent
+
+
+
+#3
+lista_integer = ["El", "casi", "sii"]
+
+print(" ".join(lista_integer))
+
+
+#5: Hide a Credit Card Number
+#Write a function that takes a credit card number and transforms it into a string where all
+#digits except the last four are replaced with asterisks.
+
+def hide_credict_card(card_number):
+    card = str(card_number)
+    return "*"*(len(card) - 4) + card[-4:]
+
+resultado = hide_credict_card(456798630883)
+print(resultado)
+
+
+
+
+#8: Is the Product Divisible by the Sum?
+
+
+
+def prueb(numeros):
+    if not numeros:
+        return False
+
+    produc = 1
+    add = 0
+
+    for num in numeros:
+        produc *= num
+        add += num
+
+    if add == 0:
+        return False
+
+    return produc % add == 0
+
+
+resultado = prueb([4, 4, 8])
+
+print(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
